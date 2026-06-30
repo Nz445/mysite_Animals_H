6562d6351d948e0af1e6357a5908d8c213a21e34
nh445 <nhzh0909@163.com>
2026-06-30 22:18:50 +0800

    测试git
    11

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <MainNav :items="navItems" active="首页" />
     <main>
       <section ref="heroRef" class="hero glass-card">
-        <div class="hero-left"><el-tag class="pill-tag pink">宠物综合服务主页</el-tag><h1>这里有你喜欢的小毛球</h1><p>发现可爱、了解性格、记录陪伴。让每一只宠物都被温柔看见，从档案、领养到社区互动，一站式认识它们的故事。</p><div class="hero-actions"><el-button type="primary" class="btn-primary">立即查看宠物</el-button><el-button class="btn-outline">了解领养信息</el-button></div></div>
+        <div class="hero-left"><el-tag class="pill-tag pink">宠物综合服务主页12124234134</el-tag><h1>这里有你喜欢的小毛球</h1><p>发现可爱、了解性格、记录陪伴。让每一只宠物都被温柔看见，从档案、领养到社区互动，一站式认识它们的故事。</p><div class="hero-actions"><el-button type="primary" class="btn-primary">立即查看宠物</el-button><el-button class="btn-outline">了解领养信息</el-button></div></div>
         <div class="hero-art"><div class="hero-art-card"><div class="pet-illustration"><div class="pet-face cat"></div><div class="pet-face dog"></div><div class="pet-face rabbit"></div></div></div></div>
       </section>
       <section class="section-title"><h2>宠物介绍</h2><p>每一只宠物都有自己的故事</p></section>
